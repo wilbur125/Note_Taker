2bcf048d6642decd6a1ab2735397810de5cec4b2
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-11 22:02:59 -0800

    Update environmental variables

---
+++ server.js
@@ -0,0 +1,18 @@
+//Dependencies
+const express = require("express");
+require('dotenv').config();
+
+//Express Configuration
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Router
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
